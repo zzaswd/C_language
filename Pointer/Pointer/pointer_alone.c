2d490aaa2c4926e0d0a654f0079aa983ef52db35
강시형 <zzaswd@naver.com>
2022-04-06 22:57:09 +0900

    포인터와 배열 예습

--- Pointer/Pointer/pointer_alone.c
+++ Pointer/Pointer/pointer_alone.c
@@ -126,3 +126,172 @@
 //
 //
 //}
+
+//int main_275p(void) {
+//
+//	int arr[5] = { 10,20,30,40,50 };
+//	int* pa = arr;
+//	int* pb = pa + 3;
+//	printf("pa : %u\n", pa);
+//	printf("pb : %u\n", pb); // pb와 pa의 차이는 sizeof(int) * 3의 차이.
+//
+//	pa++;
+//	printf("pb - pa : %u\n", pb - pa); // 2. 주소 차이는 8나고 int형 크기인 4로 나누면 2.
+//
+//	printf("앞에 있는 배열 요소의 값 출력 : ");
+//	if (pa < pb) printf("%d", *pa);
+//	else printf("%d", *pb);
+//
+//	return 0;
+//}
+
+
+
+//void print_arr(int* pa);
+//
+//int main_282p(void){
+//	int arr[5] = { 1,2,3,4,5 };
+//
+//	print_arr(arr);
+//	printf("\n");
+//	for (int i = 0; i < 5; i++) {
+//
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//
+//void print_arr(const int* pa) {
+//
+//	for (int i = 0; i < 5; i++) {
+//		//printf("%d ", *(pa + i));
+//		//pa[3] = 1000;
+//		printf("%d ", pa[i]);
+//	}
+//
+//}
+
+
+
+
+//void print_arr(int* pa, int size);
+//
+//int main_284p(void) {
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 1,2,3,4,5,6,7 };
+//
+//	print_arr(arr1, sizeof(arr1) / sizeof(arr1[1]));
+//	printf("\n");
+//	print_arr(arr2, sizeof(arr2) / sizeof(arr2[1]));
+//
+//	return 0;
+//
+//}
+//
+//void print_arr(int* pa, int size) {
+//
+//	for (int i = 0; i < size; i++) {
+//		printf("%d ", pa[i]);
+//	}
+//}
+
+
+//void input_arr(double* pa, int size);
+//double find_max(double* pa, int size);
+//
+//int main_286p(void) {
+//
+//	double arr[5];
+//	double max;
+//	int size = sizeof(arr) / sizeof(arr[1]);
+//
+//	input_arr(arr, size);
+//	max = find_max(arr, size);
+//	printf("배열의 최대값 : %.1lf\n", max);
+//	return 0;
+//
+//}
+//
+//void input_arr(double* pa, int size) {
+//
+//	for (int i = 0; i < size; i++) {
+//		scanf_s("%lf", &pa[i]);
+//	}
+//}
+//
+//double find_max(double* pa, int size) {
+//	
+//	double max = pa[0];
+//	for (int i = 1; i < size; i++) {
+//		if (pa[i] > max) max = pa[i];
+//	}
+//
+//	return max;
+//}
+
+
+//----------- 확인 문제
+//int month[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+//
+//void print_month(int* mp) {
+//	
+//	for (int i = 0; i < 12; i++) {
+//		printf("%5d", mp[i]);
+//		if ((i+1) % 5 == 0) printf("\n");
+//	}
+//}
+//
+//
+//int main(void) {
+//
+//	print_month(month);
+//}
+
+
+
+
+
+// -------------- 도전문제
+
+//void input_nums(int* lotto_nums);
+//void print_nums(int* lotto_nums);
+//
+//int main(void) {
+//
+//	int lotto_nums[6];
+//
+//	input_nums(lotto_nums);
+//	print_nums(lotto_nums);
+//
+//	return 0;
+//}
+//
+//void input_nums(int* lotto_nums) {
+//
+//	int temp;
+//	int i = 0;
+//	while (1) {
+//		if (i == 6) break;
+//		int compare = -1;
+//		scanf_s("%d", &temp);
+//		for (int j = 0; j < i; j++) {
+//			if (lotto_nums[j] == temp) {
+//				printf("같은 번호가 있습니다!\n");
+//				compare = 1;
+//				break;;
+//			}
+//		}
+//
+//		if (compare == 1) continue;
+//		else lotto_nums[i] = temp;
+//		i++;
+//		
+//	}
+//}
+//
+//void print_nums(int* lotto_nums) {
+//	printf("로또 번호 : ");
+//	for (int i = 0; i < 6; i++) {
+//		printf("%d ", lotto_nums[i]);
+//	}
+//}
